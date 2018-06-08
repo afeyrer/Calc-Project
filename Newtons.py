3014a8fb021c939d26e2acc2d92fc5d127d26ee0
afeyrer <abigailfeyrer@hanovernorwichschools.org>
2018-06-07 21:34:56 -0400

    Updated from Brython Server: 6/7/2018 9:34:58 PM

--- Newtons.py
+++ Newtons.py
@@ -1,5 +1,9 @@
 from math import *
-x
+d=0.00000001
 eq=input("put in a function: ")
-e=eval(eq)
-print(e)
+x=int(input("what is your first guess? "))
+y=10000000
+#while (y-x)<0.000001 and (y-x)>-0.000001:
+y=x
+deriv=(eval(eq(x+d)))
+print(deriv)
