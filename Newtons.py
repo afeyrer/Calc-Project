f777738d4799dd50c92fca914a307947c901d7ee
afeyrer <abigailfeyrer@hanovernorwichschools.org>
2018-06-08 12:27:31 -0400

    Updated from Brython Server: 6/8/2018 12:27:32 PM

--- Newtons.py
+++ Newtons.py
@@ -2,14 +2,15 @@ from math import *
 d=0.00000001
 eq=input("put in a function: ")
 xs=int(input("what is your first guess? "))
-y=10000000
-#while (y-xs)<0.000001 and (y-xs)>-0.000001:
-x=xs+d
-yup=(eval(eq))
-print (yup)
-x=xs-d
-yd=(eval(eq))
-print (yd)
-deriv=(yup-yd)/(2*d)
-print(round(deriv, [9]))
-
+xf=10000000
+while (xf-xs)>d or (xf-xs)<-d:
+    xf=xs
+    x=xs
+    y=(eval(eq))
+    x=xs+d
+    yup=(eval(eq))
+    x=xs-d
+    yd=(eval(eq))
+    deriv=(yup-yd)/(2*d)
+    xs=xs-(y/deriv)
+    print(xs)
