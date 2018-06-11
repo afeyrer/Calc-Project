705cd725a14bec2a4781fdd1bfca887c9e43dbba
afeyrer <abigailfeyrer@hanovernorwichschools.org>
2018-06-11 14:45:49 -0400

    Updated from Brython Server: 6/11/2018 2:45:49 PM

--- rotatedsolids.py
+++ rotatedsolids.py
@@ -1,20 +1,22 @@
 from math import *
 eq=input("This program will estimate the volume of a function rotated around the x axis or the y axis. Put in a function: ")
 k=input("x axis or y axis (type x or y)")
-xs=int(input("what is the first limit of integration? "))
+xs=float(input("what is the first limit of integration? "))
 xsa=xs
-xf=int(input("what is the second limit of integration? "))
+xf=float(input("what is the second limit of integration? "))
 d=(xf-xs)/10000
 nt=0
 nta=0
-while xs<xf and k==x:
+while xs<xf and k=="x":
     x=xs
     nt=nt+(eval(eq))**2*d*pi
     xs=xs+d
-while xsa<xf and k==y:
+while xsa<xf and k=="y":
     x=xsa
     nta=nta+(eval(eq))*2*pi*xsa*d
     xsa=xsa+d
-print("around the x: " + str(round(nt, 4)))
-print("around the y: " + str(round(nta, 4)))
+if k=="x":
+    print("around the x: " + str(round(nt, 4)))
+if k=="y":
+    print("around the y: " + str(round(nta, 4)))
     
