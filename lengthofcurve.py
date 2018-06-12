f1a0fa6aa7aded03ea22ad169cf75926d3c9cf9c
afeyrer <abigailfeyrer@hanovernorwichschools.org>
2018-06-12 14:49:50 -0400

    Updated from Brython Server: 6/12/2018 2:49:54 PM

--- lengthofcurve.py
+++ lengthofcurve.py
@@ -1,1 +1,15 @@
+from math import *
+eq=input("This program will estimate the length of a curve. Put in a function: ")
+xs=float(input("what is the leftmost x value? "))
+xf=float(input("what is the rightmost x value? "))
+d=(xf-xs)/10000
+l=0
+while xs<xf:
+    x=xs
+    y1=(eval(eq))
+    xs=xs+d
+    x=xs
+    y2=(eval(eq))
+    l=l+((y2-y1)**2+d**2)**(0.5)
+print (round(l, 4))
 
